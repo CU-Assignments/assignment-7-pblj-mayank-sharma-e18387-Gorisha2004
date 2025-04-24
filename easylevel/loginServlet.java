952dee826818557d69a46f83fec4fc43dd427e90
Gorisha2004 <157053342+Gorisha2004@users.noreply.github.com>
2025-04-24 14:01:07 +0530

    Create loginServlet.java

---
+++ easylevel/loginServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        // Simple static check for demo purposes
+        if("user".equals(username) && "password".equals(password)) {
+            request.setAttribute("username", username);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("welcome.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            response.getWriter().println("Invalid credentials, please try again.");
+        }
+    }
+}
